595d8bed1220c2a953b3a4194cb3162f2cbe81d8
triple-eh <aleckaleksandrov@gmail.com>
2020-06-08 01:06:07 -0700

    Add basic view map layout at /maps/:id

--- routes/maps.js
+++ routes/maps.js
@@ -68,32 +68,37 @@ module.exports = (db) => {
     let params = [mapName, mapDesc, parseInt(userId)];
     console.log(query, params);
     db.query(query,[mapName, mapDesc, parseInt(userId)])
-      .then(data => {
-        console.log("Added into maps:", data.rows[0]);
+    .then(data => {
+      console.log("Added into maps:", data.rows[0]);
         res.redirect("/maps");
       })
       .catch(err => {
         res
-          .status(500)
-          .json({ error: err.message });
+        .status(500)
+        .json({ error: err.message });
       });
-  });
+    });
   router.get('/:id', (req, res) => {
+    if (!req.session.userId) return res.send('Only logged in users can create maps');
+    let userId = req.session.userId;
     let query = `
     SELECT
-      maps.name as map_name, maps.description as map_desc, maps.created_by as map_by,
+      maps.name as map_name, maps.description as map_desc, maps.created_by as created_by,
       locations.title as loc_title, locations.lat as lat, locations.lng as lng, locations.description as loc_desc,
-      COUNT(favourites.id) as faves_count
+      (SELECT COUNT(*) FROM favourites WHERE map_id = maps.id and user_id = $2) as is_fav,
+      COUNT(favourites.id) as favs_count
     FROM maps
     LEFT JOIN locations ON maps.id = locations.map_id
     LEFT JOIN favourites on maps.id = favourites.map_id
     WHERE maps.id = $1
-    GROUP by 1, 2, 3, 4, 5, 6, 7
+    GROUP by 1, 2, 3, 4, 5, 6, 7, 8
     `;
-    console.log(query, req.params.id);
-    db.query(query,[parseInt(req.params.id)])
+    console.log(query, [req.params.id, userId]);
+    db.query(query,[parseInt(req.params.id),userId])
       .then(data => {
-        res.json(data.rows);
+        const apiKey = process.env.API_KEY;
+        const map = data.rows[0];
+        res.render("map", { map, apiKey });
       })
       .catch(err => {
         res
