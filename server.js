b2cd42a101ae5d3bd73e409a3b49b96e2c61087f
Alex Aleksandrov <aleckaleksandrov@gmail.com>
2022-01-13 09:23:51 -0800

    Replaced deprecated node-sass with sass package, now works ok on Windows

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
+const sassMiddleware       = require("./lib/sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
 const cookieSession    = require('cookie-session');
@@ -25,11 +25,10 @@ app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
+app.use("/styles", sassMiddleware({
+  source: __dirname + "/styles",
+  destination: __dirname + "/public/styles",
+  isSass: false
 }));
 app.use(express.static("public"));
 app.use(cookieSession({
